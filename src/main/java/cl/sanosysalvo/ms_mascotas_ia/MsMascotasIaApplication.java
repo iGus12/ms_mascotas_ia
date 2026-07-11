ec7ceffd97aac4a6e4cdc9af839e2fc78e316d2e
iGus12 <ag.varas@duocuc.cl>
2026-07-10 21:54:49 -0400

    Add files via upload

---
+++ src/main/java/cl/sanosysalvo/ms_mascotas_ia/MsMascotasIaApplication.java
@@ -0,0 +1,13 @@
+package cl.sanosysalvo.ms_mascotas_ia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsMascotasIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsMascotasIaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/sanosysalvo/ms_mascotas_ia/MsMascotasIaApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.sanosysalvo.ms_mascotas_ia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsMascotasIaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
